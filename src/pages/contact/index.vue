8bb7a676bb5ee68d0b11c7639762c9b676d88bc4
randy39 <3randy9@gmail.com>
2019-03-29 21:24:49 +0900

    fix form

--- src/pages/contact/index.vue
+++ src/pages/contact/index.vue
@@ -2,7 +2,7 @@
   <section>
     <div class="container">
       <h1>Contact</h1>
-      <form name="contact" method="POST" data-netlify="true" netlify>
+      <form name="contact" method="POST" netlify>
         <label for="name">Your Name:</label>
         <input type="text" name="name" id="name">
         <label for="email">Your Email:</label>
